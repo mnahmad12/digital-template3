23855820fbf7217e2367a4c81828cac79562198c
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-19 11:42:15 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ function preload() {
 	game.load.spritesheet('policedude','assets/cops.png');
 	game.load.image('bag','assets/money.png');
 	game.load.bitmapFont('desyrel', 'assets/desyrel.png', 'assets/desyrel.xml');
+	game.load.audio('pick','assets/pick.mp3')
+	game.load.audio('scanner','assets/scanner.mp3');
+	game.load.audio('siren','assets/siren.mp3')
 }	
 
 var wall;
@@ -17,12 +20,18 @@ var score=0;
 var text;
 var winText;
 var loseText;
-
+var actionTime=0;
+var cop2;
+var sound;
+var scanner;
+var siren;
 
 function create() {
 
-	
-	
+	scanner=game.add.audio('scanner');
+	scanner.play();
+	sound=game.add.audio('pick');
+	siren=game.add.audio('siren');
 	text = game.add.bitmapText(50, 50, 'desyrel','Collect the Money, Avoid the Cops!',32);
 	
 	game.world.setBounds(0,0,1200,600);
@@ -38,10 +47,10 @@ function create() {
     dude = game.add.sprite(game.world.centerX, game.world.centerY, 'dude');
 	
 	cop=game.add.sprite(1100,200,'policedude',0);
+	cop2=game.add.sprite(100,500,'policedude',0);
 	
 	
-	
-	for (var i = 0; i < 6; i++)
+	for (var i = 0; i < 4; i++)
     {
         //  They are evenly spaced out on the X coordinate, with a random Y coordinate
         var wallItem=wall.create(game.rnd.integerInRange(90,1110), game.rnd.integerInRange(90,510), 'wall', null);
@@ -67,6 +76,7 @@ function create() {
 	
 	game.physics.arcade.enable(bag);
 	game.physics.arcade.enable(cop);
+	game.physics.arcade.enable(cop2);
     game.physics.arcade.enable(wall);
 	game.physics.enable(dude, Phaser.Physics.ARCADE);
 	
@@ -75,11 +85,13 @@ function create() {
 	dude.body.bounce.set(1);
 	cop.body.collideWorldBounds = true;
 	cop.body.bounce.set(1);
+	cop2.body.collideWorldBounds=true;
+	cop2.body.bounce.set(1);
 	
     cursors = game.input.keyboard.createCursorKeys();
 	
 	
-
+	game.physics.arcade.moveToObject(cop2,dude, 50 );
 	
 	
 }
@@ -104,33 +116,35 @@ function update() {
 	
 	
 	
+	
+	
     if (cursors.up.isDown)
     {
 		
         dude.body.velocity.y=-150;
-		cop.body.velocity.y=-390
-		
+		cop.body.velocity.y=-500;
+		cop2.body.velocity.y=-250;
     }
     else if (cursors.down.isDown)
     {
         dude.body.velocity.y=150;
-		cop.body.velocity.y=350
-		
+		cop.body.velocity.y=500;
+		cop2.body.velocity.y=250;
     }
 
     else if (cursors.left.isDown)
     {
 		
         dude.body.velocity.x = -150;
-		cop.body.velocity.x=-330
-		
+		cop.body.velocity.x=-500;
+		cop2.body.velocity.x=-250;
     }
     else if (cursors.right.isDown)
     {
 			
 		dude.body.velocity.x=150;
-		cop.body.velocity.x=370
-		
+		cop.body.velocity.x=500;
+		cop2.body.velocity.x=250;
 		
     }
 	
@@ -144,11 +158,34 @@ function update() {
 		game.physics.arcade.collide(wall,cop);
 	}
 	
-	if(game.physics.arcade.overlap(cop,dude,null,null,this))
+	if(game.physics.arcade.overlap(cop2,wall,null,null,this))
+	{
+		game.physics.arcade.collide(wall,cop2);
+	}
+	
+	if(game.physics.arcade.overlap(cop,dude,null,null,this)&&siren.isPlaying==false)
 	{
 		//lose!
-		game.paused=true;
+		scanner.stop();
+		siren.play();
+		cop.body.velocity=0;
+		cop2.body.velocity=0;
+		dude.body.velocity=0;
+		
 		loseText = game.add.bitmapText(500, 500, 'desyrel','You Lose, Refresh=Replay!',52);
+		
+	}
+	
+	if(game.physics.arcade.overlap(cop2,dude,null,null,this)&&siren.isPlaying==false)
+	{
+		//lose!
+		scanner.stop();
+		siren.play();
+		cop.body.velocity=0;
+		cop2.body.velocity=0;
+		dude.body.velocity=0;
+		loseText = game.add.bitmapText(500, 500, 'desyrel','You Lose, Refresh=Replay!',52);
+		
 	}
 	
 	if(game.physics.arcade.overlap(dude,bag,null,null,this))
@@ -167,13 +204,15 @@ function update() {
 function collisionHandler (player, mon) {
 
 
-    
+    sound.play();
     mon.kill();
 	mon.exists=false;
     score++;
 
 }
 	
+	
+
 
 
 
